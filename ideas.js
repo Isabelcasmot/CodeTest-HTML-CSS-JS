acec54b9f15784b2154119992b1362eb2aaa9fa8
Isabelcasmot <isabelcasmot@gmail.com>
2022-05-11 00:47:39 +0100

    Final arrangements. Read Me Added.

--- ideas.js
+++ ideas.js
@@ -4,6 +4,7 @@ const containerIdeasAccepted = document.querySelector('.accepted')
 
 
 fetch('https://devtest.awesomebeta.com/api/ideas/')
+
     .then(response => response.json())
     .then(data => {
 
@@ -11,6 +12,7 @@ fetch('https://devtest.awesomebeta.com/api/ideas/')
             if (idea.status === 'open') {
                 const divOpenIdea = createDivOpenIdea(idea)
                 containerOpenIdeas.appendChild(divOpenIdea)
+
             } if (idea.status === 'in-review') {
                 const divIdeaReview = createDivReview(idea)
                 containerIdeasReview.appendChild(divIdeaReview)
@@ -20,22 +22,18 @@ fetch('https://devtest.awesomebeta.com/api/ideas/')
                 containerIdeasAccepted.appendChild(divIdeaAccepted)
 
             }
-
-
-
-
         }
-
     });
 
-// me devuelve todas las ideas, las ideas tienen un status, las de open las meto en open, las de review en review y las  aceptadas con las aceptadas. Esos arrays los tengo que print en cada div que corresponde. 
 
 function createDivOpenIdea(openIdea) {
 
     const pName = document.createElement('p');
     pName.innerText = `${openIdea.name}`;
+
     const pLike = document.createElement('p');
     pLike.innerHTML = ` <img src="./images/heart.svg"> ${openIdea.likes}  <img src="./images/comment_icon.svg"> ${openIdea.user_interactions.length}`
+
     const div = document.createElement('div');
     div.classList.add('openIdea');
 
@@ -46,38 +44,37 @@ function createDivOpenIdea(openIdea) {
     return div;
 }
 
+
 function createDivReview(ideaReview) {
 
     const pName = document.createElement('p');
     pName.innerText = `${ideaReview.name}`;
+
     const pLike = document.createElement('p');
     pLike.innerHTML = ` <img src="./images/heart.svg"> ${ideaReview.likes}  <img src="./images/comment_icon.svg"> ${ideaReview.user_interactions.length} `
+
     const div = document.createElement('div');
     div.classList.add('ideaReview');
 
     div.appendChild(pName);
     div.appendChild(pLike);
 
-
     return div;
 }
 
 function createDivAccepted(ideaAccepted) {
-    console.log('hola')
+
     const pName = document.createElement('p');
     pName.innerText = `${ideaAccepted.name}`;
+
     const pDate = document.createElement('p');
     pDate.innerText = ` ${ideaAccepted.updated_at}`
+
     const div = document.createElement('div');
     div.classList.add('ideaAccepted');
 
     div.appendChild(pName);
     div.appendChild(pDate);
 
-
     return div;
 }
-
-
-
-/* crear otro parrafo dentro del div y meter ahi el likes y el replis con los iconos */
---
+++ readme.txt
@@ -0,0 +1,15 @@
+Hello!
+
+I make this document to explain some things.
+
+I tried to find a font family similar to your logo and even searched for it on your website. In the end I used Montserrat since I did not recibe anything about it.
+
+For the user image, I used this lovely lady since I did not get any picture either (in case you are wondering why that's different)
+
+I am sorry if I got some things mixed up, for example I used px and em for some things. I do not know if I did it right, but I used em for font size because some users can change the font of their browser.
+
+I am sorry if it's not exactly the same and I hope you don't get too scared of some weird thing about the code, but I had a lot of fun doing this code test and it helped me remember some things.
+
+Thanks for the opportunity and I appreciate any feedback.
+
+-Isabel Castilla
