3afd04f61e5b715f6cdf58de53507d6450982fc4
Isabelcasmot <isabelcasmot@gmail.com>
2022-05-10 22:26:42 +0100

    Separation of ideas by JS status.
    CSS "Articles" fix

--- ideas.js
+++ ideas.js
@@ -1,4 +1,6 @@
 const containerOpenIdeas = document.querySelector('.openIdeas')
+const containerIdeasReview = document.querySelector('.review')
+const containerIdeasAccepted = document.querySelector('.accepted')
 
 
 fetch('https://devtest.awesomebeta.com/api/ideas/')
@@ -6,9 +8,20 @@ fetch('https://devtest.awesomebeta.com/api/ideas/')
     .then(data => {
 
         for (let idea of data.data[0]) {
-            // console.log(idea)
-            const divOpenIdea = createDivOpenIdea(idea)
-            containerOpenIdeas.appendChild(divOpenIdea)
+            if (idea.status === 'open') {
+                const divOpenIdea = createDivOpenIdea(idea)
+                containerOpenIdeas.appendChild(divOpenIdea)
+            } if (idea.status === 'in-review') {
+                const divIdeaReview = createDivReview(idea)
+                containerIdeasReview.appendChild(divIdeaReview)
+
+            } if (idea.status === 'accepted') {
+                const divIdeaAccepted = createDivAccepted(idea)
+                containerIdeasAccepted.appendChild(divIdeaAccepted)
+
+            }
+
+
 
 
         }
@@ -20,15 +33,51 @@ fetch('https://devtest.awesomebeta.com/api/ideas/')
 function createDivOpenIdea(openIdea) {
 
     const pName = document.createElement('p');
-    pName.innerText = `${openIdea.name} ${openIdea.likes}`;
+    pName.innerText = `${openIdea.name}`;
+    const pLike = document.createElement('p');
+    pLike.innerHTML = ` <img src="./images/heart.svg"> ${openIdea.likes}  <img src="./images/comment_icon.svg"> ${openIdea.user_interactions.length}`
     const div = document.createElement('div');
     div.classList.add('openIdea');
 
     div.appendChild(pName);
+    div.appendChild(pLike);
+    pLike.appendChild
 
     return div;
-
-
 }
+
+function createDivReview(ideaReview) {
+
+    const pName = document.createElement('p');
+    pName.innerText = `${ideaReview.name}`;
+    const pLike = document.createElement('p');
+    pLike.innerHTML = ` <img src="./images/heart.svg"> ${ideaReview.likes}  <img src="./images/comment_icon.svg"> ${ideaReview.user_interactions.length} `
+    const div = document.createElement('div');
+    div.classList.add('ideaReview');
+
+    div.appendChild(pName);
+    div.appendChild(pLike);
+
+
+    return div;
+}
+
+function createDivAccepted(ideaAccepted) {
+    console.log('hola')
+    const pName = document.createElement('p');
+    pName.innerText = `${ideaAccepted.name}`;
+    const pDate = document.createElement('p');
+    pDate.innerText = ` ${ideaAccepted.updated_at}`
+    const div = document.createElement('div');
+    div.classList.add('ideaAccepted');
+
+    div.appendChild(pName);
+    div.appendChild(pDate);
+
+
+    return div;
+}
+
+
 
 /* crear otro parrafo dentro del div y meter ahi el likes y el replis con los iconos */
