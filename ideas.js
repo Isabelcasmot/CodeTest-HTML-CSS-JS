a07ac67fc0801cea30910e28060c315100cb3e6f
Isabelcasmot <isabelcasmot@gmail.com>
2022-05-10 12:56:51 +0100

    Creation of the div with js. Added the image of right .

--- ideas.js
+++ ideas.js
@@ -1,20 +1,34 @@
-const ideas = document.querySelector('.ideasreq')
+const containerOpenIdeas = document.querySelector('.openIdeas')
+
 
 fetch('https://devtest.awesomebeta.com/api/ideas/')
     .then(response => response.json())
     .then(data => {
 
         for (let idea of data.data[0]) {
-            console.log(idea)
+            // console.log(idea)
+            const divOpenIdea = createDivOpenIdea(idea)
+            containerOpenIdeas.appendChild(divOpenIdea)
 
 
         }
 
-    })
+    });
 
 // me devuelve todas las ideas, las ideas tienen un status, las de open las meto en open, las de review en review y las  aceptadas con las aceptadas. Esos arrays los tengo que print en cada div que corresponde. 
 
-function createDivOpenIdeas(idea) {
+function createDivOpenIdea(openIdea) {
+
+    const pName = document.createElement('p');
+    pName.innerText = `${openIdea.name} ${openIdea.likes}`;
+    const div = document.createElement('div');
+    div.classList.add('openIdea');
+
+    div.appendChild(pName);
+
+    return div;
 
 
 }
+
+/* crear otro parrafo dentro del div y meter ahi el likes y el replis con los iconos */
